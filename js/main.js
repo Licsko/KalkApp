0131b1646d68d035f084511c405277ea3d350b2f
Licsko <varga.sandor@mail.com>
2020-09-27 23:11:25 +0200

    Comments has been added.

--- js/main.js
+++ js/main.js
@@ -7,99 +7,106 @@ var szaz = 100;
 
 var eredmeny = document.querySelector('.eladasi-ar-mezo');
 
+//--------EGYÉB ÁRU-------
 function otAFA() {
-    var afaSzazalek = (szaz + kulcs1)/szaz;
-    var haszonkulcs = (szaz + haszonEgyeb)/szaz;
+    var afaSzazalek = (szaz + kulcs1) / szaz;
+    var haszonkulcs = (szaz + haszonEgyeb) / szaz;
     var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr*afaSzazalek;
-    var eladasiAR = afavalNovelt*haszonkulcs;
+
+    var afavalNovelt = beszerzesiAr * afaSzazalek;
+    var eladasiAR = afavalNovelt * haszonkulcs;
     var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
 }
 
 function tNyolcAFA() {
-    var afaSzazalek = (szaz + kulcs2)/szaz;
-    var haszonkulcs = (szaz + haszonEgyeb)/szaz;
+    var afaSzazalek = (szaz + kulcs2) / szaz;
+    var haszonkulcs = (szaz + haszonEgyeb) / szaz;
     var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr*afaSzazalek;
-    var eladasiAR = afavalNovelt*haszonkulcs;
+
+    var afavalNovelt = beszerzesiAr * afaSzazalek;
+    var eladasiAR = afavalNovelt * haszonkulcs;
     var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";}
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+}
 
 function hHetAFA() {
-    var afaSzazalek = (szaz + kulcs3)/szaz;
-    var haszonkulcs = (szaz + haszonEgyeb)/szaz;
+    var afaSzazalek = (szaz + kulcs3) / szaz;
+    var haszonkulcs = (szaz + haszonEgyeb) / szaz;
     var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr*afaSzazalek;
-    var eladasiAR = afavalNovelt*haszonkulcs;
-    var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";}
 
+    var afavalNovelt = beszerzesiAr * afaSzazalek;
+    var eladasiAR = afavalNovelt * haszonkulcs;
+    var arRes = eladasiAR - afavalNovelt;
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+}
 
 //--------TEJTERMÉK-------
 function otAFATEJ() {
-    var afaSzazalek = (szaz + kulcs1)/szaz;
-    var haszonkulcs = (szaz + haszonTej)/szaz;
+    var afaSzazalek = (szaz + kulcs1) / szaz;
+    var haszonkulcs = (szaz + haszonTej) / szaz;
     var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr*afaSzazalek;
-    var eladasiAR = afavalNovelt*haszonkulcs;
+
+    var afavalNovelt = beszerzesiAr * afaSzazalek;
+    var eladasiAR = afavalNovelt * haszonkulcs;
     var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";}
- 
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+}
+
 function tNyolcAFATEJ() {
-    var afaSzazalek = (szaz + kulcs2)/szaz;
-    var haszonkulcs = (szaz + haszonTej)/szaz;
+    var afaSzazalek = (szaz + kulcs2) / szaz;
+    var haszonkulcs = (szaz + haszonTej) / szaz;
     var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr*afaSzazalek;
-    var eladasiAR = afavalNovelt*haszonkulcs;
+
+    var afavalNovelt = beszerzesiAr * afaSzazalek;
+    var eladasiAR = afavalNovelt * haszonkulcs;
     var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";}
- 
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+}
+
 
 function hHetAFATEJ() {
-    var afaSzazalek = (szaz + kulcs3)/szaz;
-    var haszonkulcs = (szaz + haszonTej)/szaz;
+    var afaSzazalek = (szaz + kulcs3) / szaz;
+    var haszonkulcs = (szaz + haszonTej) / szaz;
     var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr*afaSzazalek;
-    var eladasiAR = afavalNovelt*haszonkulcs;
-    var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";}
- 
 
+    var afavalNovelt = beszerzesiAr * afaSzazalek;
+    var eladasiAR = afavalNovelt * haszonkulcs;
+    var arRes = eladasiAR - afavalNovelt;
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+}
+
+//--------CSAK HASZONKULCCSAL-------
 function csakHaszon50() {
-    var haszonkulcs = (szaz + haszonEgyeb)/szaz;
+    var haszonkulcs = (szaz + haszonEgyeb) / szaz;
     var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr;
-    var eladasiAR = afavalNovelt*haszonkulcs;
+
+    var afavalNovelt = beszerzesiAr;
+    var eladasiAR = afavalNovelt * haszonkulcs;
     var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
-}
-function csakHaszon45() {
-    var haszonkulcs = (szaz + haszonTej)/szaz;
-    var beszerzesiAr = document.querySelector('.termek-ar').value;
-    
-    var afavalNovelt =beszerzesiAr;
-    var eladasiAR = afavalNovelt*haszonkulcs;
-    var arRes = eladasiAR - afavalNovelt;
-    
-return eredmeny.innerHTML= "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
 }
 
+function csakHaszon45() {
+    var haszonkulcs = (szaz + haszonTej) / szaz;
+    var beszerzesiAr = document.querySelector('.termek-ar').value;
+
+    var afavalNovelt = beszerzesiAr;
+    var eladasiAR = afavalNovelt * haszonkulcs;
+    var arRes = eladasiAR - afavalNovelt;
+
+    return eredmeny.innerHTML = "Eladási ár: <b>" + Number(eladasiAR.toFixed(0)) + ",-Ft</b><br><small>Árrés: <i>" + Number(arRes.toFixed(0)) + ",-Ft</i></small>";
+}
+
+//--------BEVITELI MEZŐ ÜRÍTÉSE-------
 function clearInput() {
     var beszerzesiINPUT = document.querySelector('#termek-ar');
     beszerzesiINPUT.value = "";
-  }
+}
